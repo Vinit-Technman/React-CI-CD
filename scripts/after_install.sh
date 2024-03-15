ca1c4e01d654e5251203d25ee3095e198f8b0db1
Vinit-Technman <vinit.c@technmanconsulting.com>
2024-03-15 09:29:39 -0400

    change in  after_install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
+# navigate to app folder
+cd /app
+
+# # install dependencies
+npm install
+npm run build
+cp -r build/* /var/www/html
+
+
+
+
+
+
+
+
+
+
 # Kill any servers that may be running in the background
-sudo pkill -f runserver
+# sudo pkill -f runserver
 
-# Change directory to the Django project root
-cd /home/ec2-demo/Django-demo/
+# # Change directory to the Django project root
+# cd /home/ec2-demo/Django-demo/
 
-# Install the AWS CLI
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+# # Install the AWS CLI
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install
 
 
 # Verify AWS CLI installation and configure if needed
-aws --version
+# aws --version
 # aws configure  # Uncomment this line if you need to configure AWS CLI
-sudo docker-compose up
+# sudo docker-compose up
 
-
-# navigate to app folder
-# cd /app
-
-# # install dependencies
-# npm install
-# npm run build
-# cp -r build/* /var/www/html
-# npm install pm2 -g
+npm install pm2 -g
