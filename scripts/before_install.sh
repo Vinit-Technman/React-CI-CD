56613d326e1fef8ab9deddeb4fefac8561dfe85a
Vinit-Technman <vinit.c@technmanconsulting.com>
2024-03-15 09:15:59 -0400

    change in before_install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
 # navigate to app folder
-cd /app
 
-# install node and npm
-apt-get install curl
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt-get install nodejs -y
-apt-get install npm -y
-apt install nginx -y
-ufw allow 'Nginx HTTP'
+# install CodeDeploy agent
+sudo rm -rf /home/ec2-user/install
+
+sudo yum -y update
+sudo yum -y install ruby
+sudo yum -y install wget
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+sudo chmod +x ./install 
+sudo ./install auto
+
+# update os & install python3
+sudo yum update
+# sudo yum install -y python3 python3-dev python3-pip python3-venv
+# pip install --user --upgrade virtualenv
+sudo yum install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+# cd /app
+
+# # install node and npm
+# apt-get install curl
+# curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+# apt-get install nodejs -y
+# apt-get install npm -y
+# apt install nginx -y
+# ufw allow 'Nginx HTTP'
